99a537f8bf5cd993d84ac3ccebd17696e8d28633
Mladen Čikara <mladen.cikara@gmail.com>
2018-05-16 22:40:54 +0200

    Source formatting

--- src/integration-test/java/com/mladen/cikara/oauth2/resource/server/controller/RegisterActionIntTest.java
+++ src/integration-test/java/com/mladen/cikara/oauth2/resource/server/controller/RegisterActionIntTest.java
@@ -1,6 +1,10 @@
 package com.mladen.cikara.oauth2.resource.server.controller;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+
+import com.mladen.cikara.oauth2.util.DockerComposeRuleUtil;
+import com.palantir.docker.compose.DockerComposeRule;
+
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -20,15 +24,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import com.mladen.cikara.oauth2.util.DockerComposeRuleUtil;
-import com.palantir.docker.compose.DockerComposeRule;
-
 import io.restassured.http.ContentType;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 
 /**
  * This integration test tests registering new user
- * 
+ *
  * @author Mladen Čikara <mladen.cikara@gmail.com>
  *
  */
@@ -39,108 +40,184 @@ import io.restassured.module.mockmvc.RestAssuredMockMvc;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class RegisterActionIntTest {
 
-	private static final Logger logger = LoggerFactory.getLogger(RegisterActionIntTest.class);
+  private static final Logger logger = LoggerFactory.getLogger(RegisterActionIntTest.class);
 
-	@ClassRule
-	public static DockerComposeRule docker = DockerComposeRuleUtil.getDockerComposeRule();
+  @ClassRule
+  public static DockerComposeRule docker = DockerComposeRuleUtil.getDockerComposeRule();
 
-	@Autowired
-	private MockMvc mockMvc;
+  @BeforeClass
+  public static void setupClass() throws InterruptedException {
+    DockerComposeRuleUtil.setDatabaseUrlProperty(docker);
+  }
 
-	@BeforeClass
-	public static void setupClass() throws InterruptedException {
-		DockerComposeRuleUtil.setDatabaseUrlProperty(docker);
-	}
+  @Autowired
+  private MockMvc mockMvc;
 
-	@Before
-	public void setup() throws Exception {
-		logger.debug("Configuring RestAssuredMockMvc");
+  @Before
+  public void setup() throws Exception {
+    logger.debug("Configuring RestAssuredMockMvc");
 
-		RestAssuredMockMvc.mockMvc(mockMvc);
-	}
+    RestAssuredMockMvc.mockMvc(mockMvc);
+  }
 
-	@Test
-	public void whenPostCorrectUserDataWithConfirmedPassword_thenCreated() throws Exception {
-		JSONObject jsonObj = new JSONObject().put("email", "test@test.org").put("firstName", "TestName")
-				.put("lastName", "TestSurname").put("password", "secret").put("passwordConfirmation", "secret");
+  @Test
+  public void whenPostCorrectUserDataThatAlreadyExists_thenBadRequest() throws Exception {
+    final JSONObject jsonObj =
+        new JSONObject()
+            .put("email", "admin@oauth2.com")
+            .put("firstName", "TestName")
+            .put("lastName", "TestSurname")
+            .put("password", "secret")
+            .put("passwordConfirmation", "secret");
 
-		// @formatter:off
-		final MvcResult response = given().body(jsonObj.toString()).contentType(ContentType.JSON).log().all().when()
-				.post("/register").then().log().all().statusCode(HttpStatus.CREATED.value()).extract().response()
-				.mvcResult();
-
-		logger.debug("Response: {}", response);
-		// @formatter:on
-	}
-
-	@Test
-	public void whenPostCorrectUserDataWithouthdPasswordsMatching_thenBadRequest() throws Exception {
-		JSONObject jsonObj = new JSONObject().put("email", "test@test.org").put("firstName", "TestName")
-				.put("lastName", "TestSurname").put("password", "secret").put("passwordConfirmation", "terces");
-
-		// @formatter:off
-		final MvcResult response = given().body(jsonObj.toString()).contentType(ContentType.JSON).log().all().when()
-				.post("/register").then().log().all().statusCode(HttpStatus.BAD_REQUEST.value()).extract().response()
-				.mvcResult();
-
-		logger.debug("Response: {}", response);
-		// @formatter:on
-	}
-
-	@Test
-	public void whenPostMisssingField_thenBadRequest() throws Exception {
-		JSONObject jsonObj = new JSONObject().put("email", "test@test.org").put("firstName", "TestName").put("password", "secret")
-				.put("passwordConfirmation", "secret");
-
-		// @formatter:off
-		final MvcResult response = given().body(jsonObj.toString()).contentType(ContentType.JSON).log().all().when()
-				.post("/register").then().log().all().statusCode(HttpStatus.BAD_REQUEST.value())
-				.extract().response()
-				.mvcResult();
+    // @formatter:off
+		final MvcResult response =
+		    given()
+		      .body(jsonObj.toString())
+		      .contentType(ContentType.JSON)
+		      .log().all()
+	      .when()
+				  .post("/register")
+			  .then()
+			    .log().all()
+			    .statusCode(HttpStatus.BAD_REQUEST.value())
+			    .extract().response().mvcResult();
 
 		logger.debug("Response: {}", response);
 		// @formatter:on
-	}
-	
-	@Test
-	public void whenPostFieldWitheEmptyString_thenBadRequest() throws Exception {
-		JSONObject jsonObj = new JSONObject().put("email", "test@test.org").put("firstName", "")
-				.put("lastName", "TestSurname").put("password", "secret").put("passwordConfirmation", "secret");
+  }
 
-		// @formatter:off
-		final MvcResult response = given().body(jsonObj.toString()).contentType(ContentType.JSON).log().all().when()
-				.post("/register").then().log().all().statusCode(HttpStatus.BAD_REQUEST.value()).extract().response()
-				.mvcResult();
+  @Test
+  public void whenPostCorrectUserDataWithConfirmedPassword_thenCreated() throws Exception {
+    final JSONObject jsonObj =
+        new JSONObject()
+            .put("email", "test@test.org")
+            .put("firstName", "TestName")
+            .put("lastName", "TestSurname")
+            .put("password", "secret")
+            .put("passwordConfirmation", "secret");
 
-		logger.debug("Response: {}", response);
-		// @formatter:on
-	}
-	
-	@Test
-	public void whenPostInvalidEmail_thenBadRequest() throws Exception {
-		JSONObject jsonObj = new JSONObject().put("email", "test").put("firstName", "TestName")
-				.put("lastName", "TestSurname").put("password", "secret").put("passwordConfirmation", "secret");
-
-		// @formatter:off
-		final MvcResult response = given().body(jsonObj.toString()).contentType(ContentType.JSON).log().all().when()
-				.post("/register").then().log().all().statusCode(HttpStatus.BAD_REQUEST.value()).extract().response()
-				.mvcResult();
-
-		logger.debug("Response: {}", response);
-		// @formatter:on
-	}
-	
-	@Test
-	public void whenPostCorrectUserDataThatAlreadyExists_thenBadRequest() throws Exception {
-		JSONObject jsonObj = new JSONObject().put("email", "admin@oauth2.com").put("firstName", "TestName")
-				.put("lastName", "TestSurname").put("password", "secret").put("passwordConfirmation", "secret");
-
-		// @formatter:off
-		final MvcResult response = given().body(jsonObj.toString()).contentType(ContentType.JSON).log().all().when()
-				.post("/register").then().log().all().statusCode(HttpStatus.BAD_REQUEST.value()).extract().response()
-				.mvcResult();
+    // @formatter:off
+		final MvcResult response =
+		    given()
+		      .body(jsonObj.toString())
+		      .contentType(ContentType.JSON)
+		      .log().all()
+	      .when()
+				  .post("/register")
+			  .then()
+			    .log().all()
+		      .statusCode(HttpStatus.CREATED.value())
+		      .extract().response().mvcResult();
 
 		logger.debug("Response: {}", response);
 		// @formatter:on
-	}
+  }
+
+  @Test
+  public void whenPostCorrectUserDataWithouthdPasswordsMatching_thenBadRequest() throws Exception {
+    final JSONObject jsonObj =
+        new JSONObject()
+            .put("email", "test@test.org")
+            .put("firstName", "TestName")
+            .put("lastName", "TestSurname")
+            .put("password", "secret")
+            .put("passwordConfirmation", "terces");
+
+    // @formatter:off
+		final MvcResult response =
+		    given()
+		      .body(jsonObj.toString())
+	        .contentType(ContentType.JSON)
+	        .log().all()
+        .when()
+				  .post("/register")
+			  .then()
+			    .log().all()
+			    .statusCode(HttpStatus.BAD_REQUEST.value())
+			    .extract().response().mvcResult();
+
+		logger.debug("Response: {}", response);
+		// @formatter:on
+  }
+
+  @Test
+  public void whenPostFieldWitheEmptyString_thenBadRequest() throws Exception {
+    final JSONObject jsonObj =
+        new JSONObject()
+            .put("email", "test@test.org")
+            .put("firstName", "")
+            .put("lastName", "TestSurname")
+            .put("password", "secret")
+            .put("passwordConfirmation", "secret");
+
+    // @formatter:off
+		final MvcResult response =
+		    given()
+		      .body(jsonObj.toString())
+		      .contentType(ContentType.JSON)
+		      .log().all()
+	      .when()
+				  .post("/register")
+			  .then()
+			    .log().all()
+			    .statusCode(HttpStatus.BAD_REQUEST.value())
+			    .extract().response().mvcResult();
+
+		logger.debug("Response: {}", response);
+		// @formatter:on
+  }
+
+  @Test
+  public void whenPostInvalidEmail_thenBadRequest() throws Exception {
+    final JSONObject jsonObj =
+        new JSONObject()
+            .put("email", "test")
+            .put("firstName", "TestName")
+            .put("lastName", "TestSurname")
+            .put("password", "secret")
+            .put("passwordConfirmation", "secret");
+
+    // @formatter:off
+		final MvcResult response =
+		    given()
+		      .body(jsonObj.toString())
+		      .contentType(ContentType.JSON)
+		      .log().all()
+	      .when()
+				  .post("/register")
+			  .then()
+			    .log().all()
+		      .statusCode(HttpStatus.BAD_REQUEST.value())
+		      .extract().response().mvcResult();
+
+		logger.debug("Response: {}", response);
+		// @formatter:on
+  }
+
+  @Test
+  public void whenPostMisssingField_thenBadRequest() throws Exception {
+    final JSONObject jsonObj =
+        new JSONObject()
+            .put("email", "test@test.org")
+            .put("firstName", "TestName")
+            .put("password", "secret")
+            .put("passwordConfirmation", "secret");
+
+    // @formatter:off
+		final MvcResult response =
+		    given()
+		      .body(jsonObj.toString())
+	        .contentType(ContentType.JSON)
+	        .log().all()
+        .when()
+				  .post("/register")
+			  .then()
+			    .log().all()
+			    .statusCode(HttpStatus.BAD_REQUEST.value())
+			    .extract().response().mvcResult();
+
+		logger.debug("Response: {}", response);
+		// @formatter:on
+  }
 }
--- src/integration-test/java/com/mladen/cikara/oauth2/resource/server/controller/TestControllerWithMockMvcIntTest.java
+++ src/integration-test/java/com/mladen/cikara/oauth2/resource/server/controller/TestControllerWithMockMvcIntTest.java
@@ -12,6 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.mladen.cikara.oauth2.util.DockerComposeRuleUtil;
 import com.mladen.cikara.oauth2.util.OAuth2AuthorizationBuilder;
 import com.palantir.docker.compose.DockerComposeRule;
+
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -34,84 +35,109 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class TestControllerWithMockMvcIntTest {
 
-	private static final String PASSWORD = "secret";
+  private static final String PASSWORD = "secret";
 
-	private static final String USERNAME = "user@oauth2.com";
+  private static final String USERNAME = "user@oauth2.com";
 
-	private static final Logger logger = LoggerFactory.getLogger(TestControllerWithMockMvcIntTest.class);
+  private static final Logger logger =
+      LoggerFactory.getLogger(TestControllerWithMockMvcIntTest.class);
 
-	@ClassRule
-	public static DockerComposeRule docker = DockerComposeRuleUtil.getDockerComposeRule();
+  @ClassRule
+  public static DockerComposeRule docker = DockerComposeRuleUtil.getDockerComposeRule();
 
-	@BeforeClass
-	public static void setupClass() throws InterruptedException {
-		DockerComposeRuleUtil.setDatabaseUrlProperty(docker);
-	}
+  @BeforeClass
+  public static void setupClass() throws InterruptedException {
+    DockerComposeRuleUtil.setDatabaseUrlProperty(docker);
+  }
 
-	@Autowired
-	private MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
 
-	private String getJwt() throws Exception {
-		// @formatter:off
-		final String jwt = OAuth2AuthorizationBuilder.oauth2Request(mockMvc).grantType("password")
-				.accessTokenUrl("/oauth/token").username(USERNAME).password(PASSWORD)
-				.clientId("d4486b29-7f28-43db-8d4e-44df6b5785c9").clientSecret("a6f59937-fc55-485c-bf91-c8bcdaae2e45")
-				.scope("bla").getAccessToken();
+  private String getJwt() throws Exception {
+    // @formatter:off
+		final String jwt =
+		    OAuth2AuthorizationBuilder
+		      .oauth2Request(mockMvc)
+		      .grantType("password")
+		      .accessTokenUrl("/oauth/token")
+		      .username(USERNAME)
+		      .password(PASSWORD)
+		      .clientId("d4486b29-7f28-43db-8d4e-44df6b5785c9")
+		      .clientSecret("a6f59937-fc55-485c-bf91-c8bcdaae2e45")
+		      .scope("bla")
+		      .getAccessToken();
 		// @formatter:on
 
-		return jwt;
-	}
+    return jwt;
+  }
 
-	@Test
-	public void whenGetActuatorHealthWithAuthentication_thenOk() {
+  @Test
+  public void whenGetActuatorHealthWithAuthentication_thenOk() {
 
-		String jwt;
-		try {
-			jwt = getJwt();
+    String jwt;
+    try {
+      jwt = getJwt();
 
-			logger.debug("JWT {}", jwt);
+      logger.debug("JWT {}", jwt);
 
-			// @formatter:off
-			mockMvc.perform(get("/actuator/health").header("Authorization", "Bearer " + jwt)).andDo(print())
-					.andExpect(status().isOk()).andExpect(jsonPath("$.status", is("UP")));
+      // @formatter:off
+			mockMvc
+			  .perform(get("/actuator/health")
+			      .header("Authorization", "Bearer " + jwt))
+			      .andDo(print())
+			      .andExpect(status().isOk())
+			      .andExpect(jsonPath("$.status", is("UP")));
 			// @formatter:on
-		} catch (final Exception e) {
-			fail("Failed getting JWT. {}", e.getMessage());
-		}
-	}
+    } catch (final Exception e) {
+      fail("Failed getting JWT. {}", e.getMessage());
+    }
+  }
 
-	@Test
-	public void whenGetActuatorHealthWithNoAuthentication_thenUnauthorized() throws Exception {
-		// @formatter:off
-		mockMvc.perform(get("/actuator/health")).andDo(print()).andExpect(status().isUnauthorized());
+  @Test
+  public void whenGetActuatorHealthWithNoAuthentication_thenUnauthorized() throws Exception {
+    // @formatter:off
+		mockMvc
+		  .perform(get("/actuator/health"))
+		    .andDo(print())
+		    .andExpect(status().isUnauthorized());
 		// @formatter:on
-	}
+  }
 
-	@Test
-	public void whenGetPrivateHomeWithAuthentication_thenOk() throws Exception {
+  @Test
+  public void whenGetPrivateHomeWithAuthentication_thenOk() throws Exception {
 
-		final String jwt = getJwt();
+    final String jwt = getJwt();
 
-		logger.debug("JWT {}", jwt);
+    logger.debug("JWT {}", jwt);
 
-		// @formatter:off
-		mockMvc.perform(get("/private").header("Authorization", "Bearer " + jwt)).andDo(print())
-				.andExpect(status().isOk()).andExpect(content().string(containsString("Hello from the private side")));
+    // @formatter:off
+		mockMvc
+		  .perform(get("/private")
+		      .header("Authorization", "Bearer " + jwt))
+		      .andDo(print())
+		      .andExpect(status().isOk())
+		      .andExpect(content().string(containsString("Hello from the private side")));
 		// @formatter:on
-	}
+  }
 
-	@Test
-	public void whenGetPrivateHomeWithNoAuthentication_thenUnauthorized() throws Exception {
-		// @formatter:off
-		mockMvc.perform(get("/private")).andDo(print()).andExpect(status().isUnauthorized());
+  @Test
+  public void whenGetPrivateHomeWithNoAuthentication_thenUnauthorized() throws Exception {
+    // @formatter:off
+		mockMvc
+		  .perform(get("/private"))
+		  .andDo(print())
+		  .andExpect(status().isUnauthorized());
 		// @formatter:on
-	}
+  }
 
-	@Test
-	public void whenGetPublicHomeWithNoAuthentication_thenOk() throws Exception {
-		// @formatter:off
-		mockMvc.perform(get("/public")).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().string(containsString("Hello from the public side")));
+  @Test
+  public void whenGetPublicHomeWithNoAuthentication_thenOk() throws Exception {
+    // @formatter:off
+		mockMvc
+		  .perform(get("/public"))
+		  .andDo(print())
+		  .andExpect(status().isOk())
+		  .andExpect(content().string(containsString("Hello from the public side")));
 		// @formatter:on
-	}
+  }
 }

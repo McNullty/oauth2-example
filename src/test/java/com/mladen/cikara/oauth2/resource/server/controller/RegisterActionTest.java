e5392d870721452a4abcd9ff92f6816d3b869ad3
Mladen Čikara <mladen.cikara@gmail.com>
2018-06-15 20:35:05 +0200

    Created new unit test

---
+++ src/test/java/com/mladen/cikara/oauth2/resource/server/controller/RegisterActionTest.java
@@ -0,0 +1,14 @@
+package com.mladen.cikara.oauth2.resource.server.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RegisterActionTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}

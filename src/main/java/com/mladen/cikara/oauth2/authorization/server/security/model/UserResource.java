f1ea41daeb757db9c895a289ca0ee6ba091ea26a
Mladen Čikara <mladen.cikara@gmail.com>
2018-06-22 12:55:34 +0200

    Removing bug reported by FindBugs

--- src/main/java/com/mladen/cikara/oauth2/authorization/server/security/model/UserResource.java
+++ src/main/java/com/mladen/cikara/oauth2/authorization/server/security/model/UserResource.java
@@ -14,7 +14,37 @@ public class UserResource extends ResourceSupport {
     // this.add(linkTo());
   }
 
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (!super.equals(obj)) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    final UserResource other = (UserResource) obj;
+    if (user == null) {
+      if (other.user != null) {
+        return false;
+      }
+    } else if (!user.equals(other.user)) {
+      return false;
+    }
+    return true;
+  }
+
   public User getUser() {
     return user;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = super.hashCode();
+    result = prime * result + ((user == null) ? 0 : user.hashCode());
+    return result;
   }
 }

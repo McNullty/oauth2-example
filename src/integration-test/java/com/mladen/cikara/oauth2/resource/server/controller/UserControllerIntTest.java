722ab881a2012cb457aae7aa93c496d0948f11bb
Mladen Čikara <mladen.cikara@gmail.com>
2018-06-28 21:11:27 +0200

    User object is passed to /me method

--- src/integration-test/java/com/mladen/cikara/oauth2/resource/server/controller/UserControllerIntTest.java
+++ src/integration-test/java/com/mladen/cikara/oauth2/resource/server/controller/UserControllerIntTest.java
@@ -1,10 +1,14 @@
 package com.mladen.cikara.oauth2.resource.server.controller;
 
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.mladen.cikara.oauth2.authorization.server.security.model.UserDto;
+import com.mladen.cikara.oauth2.authorization.server.security.service.UserService;
 import com.mladen.cikara.oauth2.util.DockerComposeRuleUtil;
+import com.mladen.cikara.oauth2.util.OAuth2AuthorizationBuilder;
 import com.palantir.docker.compose.DockerComposeRule;
 
 import org.junit.Before;
@@ -18,10 +22,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 
@@ -51,11 +57,71 @@ public class UserControllerIntTest {
   @Autowired
   private MockMvc mockMvc;
 
+  @Autowired
+  private UserService userService;
+
+  private void createNewUser() {
+    final UserDto userDto = new UserDto.Builder()
+        .email("test@oauth2.com")
+        .firstName("testFirstName")
+        .lastName("testLastName")
+        .password("secret")
+        .passwordConfirmation("secret")
+        .build();
+
+    userService.registerUser(userDto);
+  }
+
+  private String getAuthorization() throws Exception {
+    // @formatter:off
+    final String jwt =
+        OAuth2AuthorizationBuilder
+          .oauth2Request(mockMvc)
+          .grantType("password")
+          .accessTokenUrl("/oauth/token")
+          .username("test@oauth2.com")
+          .password("secret")
+          .clientId("d4486b29-7f28-43db-8d4e-44df6b5785c9")
+          .clientSecret("a6f59937-fc55-485c-bf91-c8bcdaae2e45")
+          .scope("web")
+          .getAccessToken();
+    // @formatter:on
+
+    return jwt;
+  }
+
   @Before
   public void setup() throws Exception {
     logger.debug("Configuring RestAssuredMockMvc");
 
     RestAssuredMockMvc.mockMvc(mockMvc);
+  }
+
+  @Test
+  public void whenGetMeWhenLoggedIn_thenOK() throws Exception {
+
+    createNewUser();
+
+    final String jwt = getAuthorization();
+
+    logger.debug("Got authorization: {}", jwt);
+
+    // @formatter:off
+    final MvcResult response =
+        given()
+          .header("Authorization", "Bearer " + jwt)
+          .log().all()
+        .when()
+          .get("/me")
+        .then()
+          .log().all()
+          .statusCode(HttpStatus.OK.value())
+          .extract().response()
+          // TODO: add more assertions
+          .mvcResult();
+    // @formatter:on
+
+    logger.debug("Response: {}", response);
   }
 
   @Test
--- src/main/java/com/mladen/cikara/oauth2/resource/server/controller/UserController.java
+++ src/main/java/com/mladen/cikara/oauth2/resource/server/controller/UserController.java
@@ -2,16 +2,37 @@ package com.mladen.cikara.oauth2.resource.server.controller;
 
 import com.mladen.cikara.oauth2.authorization.server.security.model.User;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+  private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
   @GetMapping(path = "/me")
-  public ResponseEntity<User> currentUser(@AuthenticationPrincipal User currentUser) {
+  public ResponseEntity<User> currentUser(
+      @AuthenticationPrincipal org.springframework.security.core.userdetails.User currentUser,
+      @RequestHeader(value = "Authorization") String authorization) {
+    logger.debug("Authentication principal: {}", currentUser);
+    logger.debug("Authorization: {}", authorization);
+
+    // final Authentication authentication =
+    // SecurityContextHolder.getContext().getAuthentication();
+    //
+    // logger.debug("Authentication: {}", authentication);
+    //
+    // final org.springframework.security.core.userdetails.User user =
+    // (org.springframework.security.core.userdetails.User)
+    // authentication.getPrincipal();
+    //
+    // logger.debug("User: {}", user);
+
     return null;
   }
 }

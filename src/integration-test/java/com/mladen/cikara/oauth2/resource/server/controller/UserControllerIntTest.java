0ffe34db0530c4d150abd3df317ea00783b1173e
Mladen Čikara <mladen.cikara@gmail.com>
2018-06-28 13:06:32 +0200

    Test when user is not logged in

---
+++ src/integration-test/java/com/mladen/cikara/oauth2/resource/server/controller/UserControllerIntTest.java
@@ -0,0 +1,70 @@
+package com.mladen.cikara.oauth2.resource.server.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.mladen.cikara.oauth2.util.DockerComposeRuleUtil;
+import com.palantir.docker.compose.DockerComposeRule;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+
+/**
+ * This integration test tests all user end points
+ *
+ * @author mladen
+ *
+ */
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@DirtiesContext
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class UserControllerIntTest {
+
+  private static final Logger logger = LoggerFactory.getLogger(UserControllerIntTest.class);
+
+  @ClassRule
+  public static DockerComposeRule docker = DockerComposeRuleUtil.getDockerComposeRule();
+
+  @BeforeClass
+  public static void setupClass() throws InterruptedException {
+    DockerComposeRuleUtil.setDatabaseUrlProperty(docker);
+  }
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Before
+  public void setup() throws Exception {
+    logger.debug("Configuring RestAssuredMockMvc");
+
+    RestAssuredMockMvc.mockMvc(mockMvc);
+  }
+
+  @Test
+  public void whenGetMeWithoutBeingLoggedIn_thenUnauthorized() throws Exception {
+    // @formatter:off
+    mockMvc
+      .perform(get("/me"))
+        .andDo(print())
+        .andExpect(status().isUnauthorized());
+    // @formatter:on
+  }
+}

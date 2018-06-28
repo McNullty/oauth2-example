c6bfce2c106d0784f36e3cd7f266e556ea0be9c9
Mladen Čikara <mladen.cikara@gmail.com>
2018-06-28 13:06:12 +0200

    Created new controller and endpoint /me

---
+++ src/main/java/com/mladen/cikara/oauth2/resource/server/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.mladen.cikara.oauth2.resource.server.controller;
+
+import com.mladen.cikara.oauth2.authorization.server.security.model.User;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+  @GetMapping
+  public ResponseEntity<User> currentUser(@AuthenticationPrincipal User currentUser) {
+    return null;
+  }
+}

b4cd4f80c1500b5d7dabcaef941635fa6365c717
Mladen Čikara <mladen.cikara@gmail.com>
2018-07-28 11:08:25 +0200

    Spring REST docs for simple TestController

--- src/integration-test/java/com/mladen/cikara/oauth2/resource/server/controller/TestControllerWithRestAssuredIntTest.java
+++ src/integration-test/java/com/mladen/cikara/oauth2/resource/server/controller/TestControllerWithRestAssuredIntTest.java
@@ -1,19 +1,24 @@
 package com.mladen.cikara.oauth2.resource.server.controller;
 
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
 
 import com.mladen.cikara.oauth2.authorization.server.security.model.User;
 import com.mladen.cikara.oauth2.authorization.server.security.service.AuthorizationsUtilService;
 import com.mladen.cikara.oauth2.util.DockerComposeRuleUtil;
 import com.palantir.docker.compose.DockerComposeRule;
 
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
-import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import io.restassured.specification.RequestSpecification;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -22,12 +27,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
@@ -47,11 +52,16 @@ public class TestControllerWithRestAssuredIntTest {
     DockerComposeRuleUtil.setDatabaseUrlProperty(docker);
   }
 
-  @Autowired
-  private MockMvc mockMvc;
+  @Rule
+  public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
+
+  @LocalServerPort
+  private int port;
 
   @Autowired
   private AuthorizationsUtilService authorizationsUtilService;
+
+  private RequestSpecification spec;
 
   private String getJwt() throws Exception {
     final User user = this.authorizationsUtilService.getBasicUser();
@@ -68,24 +78,27 @@ public class TestControllerWithRestAssuredIntTest {
   public void setup() throws Exception {
     logger.debug("Configuring RestAssuredMockMvc");
 
-    RestAssuredMockMvc.mockMvc(this.mockMvc);
+    RestAssured.port = this.port;
+    RestAssured.baseURI = "https://localhost";
+    RestAssured.useRelaxedHTTPSValidation();
+
+    this.spec = new RequestSpecBuilder().addFilter(
+        documentationConfiguration(this.restDocumentation))
+        .build();
   }
 
   @Test
   public void whenGetActuatorHealtNoAuthentication_thenUnauthorized() throws Exception {
 
     // @formatter:off
-    final MvcResult response =
-        given()
-            .log().all()
-          .when()
-            .get("/actuator/health")
-          .then()
-            .log().all()
-            .statusCode(HttpStatus.UNAUTHORIZED.value())
-            .extract().response().mvcResult();
+    given(this.spec)
+        .log().all()
+      .when()
+        .get("/actuator/health")
+      .then()
+        .log().all()
+        .statusCode(HttpStatus.UNAUTHORIZED.value());
 
-    logger.debug("Response: {}", response);
     // @formatter:on
   }
 
@@ -97,19 +110,18 @@ public class TestControllerWithRestAssuredIntTest {
     logger.debug("JWT {}", jwt);
 
     // @formatter:off
-    final MvcResult response =
-        given()
-            .header("Authorization", "Bearer " + jwt)
-            .log().all()
-          .when()
-            .get("/actuator/health")
-          .then()
-            .log().all()
-            .statusCode(HttpStatus.OK.value())
-            .body("status", containsString("UP"))
-            .extract().response().mvcResult();
+    given(this.spec)
+        .accept(ContentType.JSON)
+        .filter(document("actuator/health"))
+        .header("Authorization", "Bearer " + jwt)
+        .log().all()
+      .when()
+        .get("/actuator/health")
+      .then()
+        .log().all()
+        .statusCode(HttpStatus.OK.value())
+        .body("status", containsString("UP"));
 
-    logger.debug("Response: {}", response);
     // @formatter:on
   }
 
@@ -121,20 +133,19 @@ public class TestControllerWithRestAssuredIntTest {
     logger.debug("JWT {}", jwt);
 
     // @formatter:off
-    final MvcResult response =
-        given()
-            .header("Authorization", "Bearer " + jwt)
-            .log().all()
-          .when()
-            .get("/private")
-          .then()
-            .log().all()
-            .statusCode(HttpStatus.OK.value())
-            .contentType(ContentType.TEXT)
-            .body(containsString("Hello from the private side"))
-            .extract().response().mvcResult();
+    given(this.spec)
+        .accept(ContentType.TEXT)
+        .filter(document("private-test"))
+        .header("Authorization", "Bearer " + jwt)
+        .log().all()
+      .when()
+        .get("/private")
+      .then()
+        .log().all()
+        .statusCode(HttpStatus.OK.value())
+        .contentType(ContentType.TEXT)
+        .body(containsString("Hello from the private side"));
 
-    logger.debug("Response: {}", response);
     // @formatter:on
   }
 
@@ -142,17 +153,16 @@ public class TestControllerWithRestAssuredIntTest {
   public void whenGetPrivateHomeWithNoAuthentication_thenUnauthorized() throws Exception {
 
     // @formatter:off
-    final MvcResult response =
-        given()
-            .log().all()
-          .when()
-            .get("/private")
-          .then()
-            .log().all()
-            .statusCode(HttpStatus.UNAUTHORIZED.value())
-            .extract().response().mvcResult();
+    given(this.spec)
+        .accept(ContentType.JSON)
+        .filter(document("private-test-1"))
+        .log().all()
+      .when()
+        .get("/private")
+      .then()
+        .log().all()
+        .statusCode(HttpStatus.UNAUTHORIZED.value());
 
-    logger.debug("Response: {}", response);
     // @formatter:on
   }
 
@@ -160,19 +170,18 @@ public class TestControllerWithRestAssuredIntTest {
   public void whenGetPublicHomeWithNoAuthentication_thenOk() throws Exception {
 
     // @formatter:off
-    final MvcResult response =
-        given()
-            .log().all()
-          .when()
-            .get("/public")
-          .then()
-            .log().all()
-            .statusCode(HttpStatus.OK.value())
-            .contentType(ContentType.TEXT)
-            .body(containsString("Hello from the public side"))
-            .extract().response().mvcResult();
+    given(this.spec)
+        .accept(ContentType.TEXT)
+        .filter(document("public-test"))
+        .log().all()
+      .when()
+        .get("/public")
+      .then()
+        .log().all()
+        .statusCode(HttpStatus.OK.value())
+        .contentType(ContentType.TEXT)
+        .body(containsString("Hello from the public side"));
 
-    logger.debug("Response: {}", response);
     // @formatter:on
 
   }
